39c3dd095e8c37a1b39215c42696cddb711fe503
Dani G <daniellegarcia@MacBook-Air-2.local>
2018-06-26 18:06:15 -0400

    homework submission

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # giphyAPI
+
+Danielle Garcia
+26 June 2018 
+Georgia Tech Bootcamp 
+
+### Homework- Assignment #06
+
+### Griphy API
+    This application first presents the user with a list of animal buttons. The user then has the 
+    chance to enter any animal in the input field. They click the 'add' button and their new animal
+    button gets added to the rest of the buttons. They can click on any button and the Giphy API pulls
+    in gifs of that animal.
+    
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,98 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    var newAnimals = ["dog", "cat"];
+
+    function displayInfo() {
+        $("button").on("click", function () {
+            var animal = $(this).attr("data-animal");
+
+            var gihpyApiUrl = "http://api.giphy.com/v1/gifs/search?q=" + animal +
+                "&api_key=T16BixY68ApEFJwpLt9pj0ic2xZAnGm3&limit=20";  
+
+            $.ajax({
+                url: gihpyApiUrl,
+                method: 'GET'
+            }).then(function (response) {
+                var data = response.data;
+
+                for (var i = 0; i < data.length; i++) {
+
+                    var animalDiv = $("<div class='animal'>");
+
+                    var rating = data[i].rating;
+                    var temp = $("<p>").text("Rating: " + data[i].rating);
+                    animalDiv.append(temp);
+
+                    var image = $("<img>").attr("src", data[i].images.fixed_height.url);
+
+                    image.addClass("gif");
+                    image.attr("src", data[i].images.fixed_height.url);
+                    image.attr("data-still",  data[i].images.fixed_height_still.url);
+                    image.attr("data-animate", data[i].images.fixed_height.url);
+                    image.attr("data-state", "still");
+
+                    animalDiv.append(image);
+                    $(".animalOptions").prepend(animalDiv);
+
+                    $(".gif").on("click", function() {
+                        var state = $(this).attr("data-state");
+
+                        if (state === "still") {
+                            $(this).attr("src", $(this).attr("data-animate"));
+                            $(this).attr("data-state", "animate");
+                        } else {
+                            $(this).attr("src", $(this).attr("data-still"));
+                            $(this).attr("data-state", "still");
+                    };
+
+                });
+            }
+
+            });
+
+        });
+
+    };
+
+    function renderButtons() {
+        $(".animalButtons").empty();
+
+        for (var i = 0; i < newAnimals.length; i++) {
+            var newBtn = $("<button>");
+            newBtn.addClass("animal5");
+            newBtn.attr("data-animal", newAnimals[i]);
+            newBtn.text(newAnimals[i]);
+            $(".animalButtons").append(newBtn);
+        }
+    }
 
 
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
 
+        var animal1 = $("#animal-input").val().trim();
+        newAnimals.push(animal1);
 
+        renderButtons();
+    });
 
+    $(document).on("click", ".animal5", displayInfo);
 
-
-
-
+    renderButtons();
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
